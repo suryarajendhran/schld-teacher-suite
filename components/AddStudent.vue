42790dc26a8df4cd3efe6c77c78f0f7eb2358a15
Surya Rajendhran <cielograndesurya@gmail.com>
2020-11-15 12:51:42 +0530

    Init AddStudent.vue and add to index

---
+++ components/AddStudent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="section">
+      <h1 class="title">{{ title }}</h1>
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Text input" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    title() {
+      return 'Add Student'
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,13 @@
 <template>
-<div class="container">
-  <div>
-    <Logo />
-    <h1 class="title">schld-teacher-suite</h1>
-    <div class="links">
-      <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer" class="button--green">
-        Documentation
-      </a>
-      <a href="https://github.com/nuxt/nuxt.js" target="_blank" rel="noopener noreferrer" class="button--grey">
-        GitHub
-      </a>
-    </div>
-  </div>
+<div>
+  <add-student/>
 </div>
 </template>
 
 <script>
-export default {}
+import AddStudent from '~/components/AddStudent.vue'
+export default {
+  components: { AddStudent },}
 </script>
 
 <style>

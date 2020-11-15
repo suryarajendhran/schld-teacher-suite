17ab416901d9f6d3e5fe4a5cdb82d3b9b22ccd67
Avinash MG <hatman290@gmail.com>
2020-11-15 16:55:05 +0530

    add question part2

--- components/AddQuestions.vue
+++ components/AddQuestions.vue
@@ -6,23 +6,45 @@
         <div class="field">
           <label class="label">Text</label>
           <div class="control">
-            <textarea class="textarea" 
-            placeholder="Textarea"
-            v-model="question"
+            <textarea
+              class="textarea"
+              placeholder="Textarea"
+              v-model="question"
             ></textarea>
           </div>
         </div>
         <div class="field is-horizontal">
-          <label class="label is">Correct Choice</label>
-          <div class="control is-half">
-            <div class="select">
-              <select v-model="correct_choice">
-                <option disabled value="default">Select the Correct Choice</option>
+          <label class="field-label">Correct Choice</label>
+          <div class="control">
+            <div class="select is-fullwidth">
+              <select class="field-body" v-model="correct_choice">
+                <option disabled value="default">
+                  Select the Correct Choice
+                </option>
                 <option v-for="choice in choices" :key="choice">
                   {{ choice }}
                 </option>
               </select>
             </div>
+          </div>
+        </div>
+        <div class="field is-horizontal">
+          <label class="field-label">Weightage</label>
+          <div class="control">
+            <input
+              class="input field-body"
+              type="tel"
+              placeholder="Option 1"
+              v-model="weightage"
+            />
+          </div>
+        </div>
+        <div class="field is-horizontal">
+          <div class="control field-body">
+            <button  class="button  is-primary">Previous</button>
+          </div>
+          <div class="control field-body">
+            <button class="button  is-primary">Previous</button>
           </div>
         </div>
       </div>
@@ -41,36 +63,28 @@
         <div class="field">
           <label class="label">Option 2</label>
           <div class="control">
-            <input
-              class="input"
-              type="tel"
-              placeholder="Option 2"
-              v-model="opt2"
-            />
+            <input class="input" placeholder="Option 2" v-model="opt2" />
           </div>
         </div>
         <div class="field">
           <label class="label">Option 3</label>
           <div class="control">
-            <input
-              class="input"
-              type="tel"
-              placeholder="Option 3"
-              v-model="opt3"
-            />
+            <input class="input" placeholder="Option 3" v-model="opt3" />
           </div>
         </div>
         <div class="field">
           <label class="label">Option 4</label>
           <div class="control">
-            <input
-              class="input"
-              type="tel"
-              placeholder="Option 4"
-              v-model="opt4"
-            />
+            <input class="input" placeholder="Option 4" v-model="opt4" />
           </div>
         </div>
+        
+        <div class="field">
+          <div class="control">
+            <button class="button is-primary">Finish</button>
+          </div>
+        </div>
+        
       </div>
     </div>
   </div>
@@ -85,7 +99,7 @@ export default {
       opt2: null,
       opt3: null,
       opt4: null,
-      choices: [this.opt1],
+      choices: [],
       year: 'default',
     }
   },

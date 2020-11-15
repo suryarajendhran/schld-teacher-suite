1ab83627a31dd1a79c7487117dd49b29d041f411
Surya Rajendhran <cielograndesurya@gmail.com>
2020-11-15 17:21:04 +0530

    Fix (sort of)

--- components/AddQuestions.vue
+++ components/AddQuestions.vue
@@ -13,57 +13,6 @@
             ></textarea>
           </div>
         </div>
-        <div class="field is-horizontal">
-          <div class="field-label is-normal">
-            <label class="label">Correct Answer</label>
-          </div>
-          <div class="field-body">
-            <div class="field">
-              <div class="control">
-                <input
-                  class="input"
-                  type="text"
-                  placeholder="e.g. Partnership opportunity"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- <div class="field is-horizontal">
-          <label class="field-label">Correct Choice</label>
-          <div class="control">
-            <div class="select is-fullwidth">
-              <select class="field-body" v-model="correct_choice">
-                <option disabled value="default">
-                  Select the Correct Choice
-                </option>
-                <option v-for="choice in choices" :key="choice">
-                  {{ choice }}
-                </option>
-              </select>
-            </div>
-          </div>
-        </div> -->
-        <!-- <div class="field is-horizontal">
-          <label class="field-label">Weightage</label>
-          <div class="control">
-            <input
-              class="input field-body"
-              type="tel"
-              placeholder="Option 1"
-              v-model="weightage"
-            />
-          </div>
-        </div> -->
-        <!-- <div class="field is-horizontal">
-          <div class="control field-body">
-            <button class="button is-primary">Previous</button>
-          </div>
-          <div class="control field-body">
-            <button class="button is-primary">Previous</button>
-          </div>
-        </div> -->
       </div>
       <div class="column is-half">
         <div class="field">
@@ -71,7 +20,7 @@
           <div class="control">
             <input
               class="input"
-              type="tel"
+              type="text"
               placeholder="Option 1"
               v-model="opt1"
             />
@@ -80,9 +29,42 @@
         <div class="field">
           <label class="label">Option 2</label>
           <div class="control">
-            <input class="input" placeholder="Option 2" v-model="opt2" />
+            <input
+              class="input"
+              type="text"
+              placeholder="Option 2"
+              v-model="opt2"
+            />
           </div>
         </div>
+      </div>
+      <div class="column is-half">
+        <div class="field">
+          <label class="label">Correct Option</label>
+          <div class="control">
+            <div class="select is-fullwidth">
+              <select v-model="correct_option">
+                <option disabled value="default">Select Correct Option</option>
+                <option v-for="choice in options" :key="choice">
+                  {{ choice }}
+                </option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Weightage</label>
+          <div class="control">
+            <input
+              class="input"
+              type="text"
+              placeholder="Marks assigned for question"
+              v-model="weightage"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="column is-half">
         <div class="field">
           <label class="label">Option 3</label>
           <div class="control">
@@ -93,12 +75,6 @@
           <label class="label">Option 4</label>
           <div class="control">
             <input class="input" placeholder="Option 4" v-model="opt4" />
-          </div>
-        </div>
-
-        <div class="field">
-          <div class="control">
-            <button class="button is-primary">Finish</button>
           </div>
         </div>
       </div>
@@ -115,7 +91,8 @@ export default {
       opt2: null,
       opt3: null,
       opt4: null,
-      choices: ['opt1', 'opt2', 'opt3'],
+      options: ['opt1', 'opt2', 'opt3'],
+      correct_option: 'default',
       year: 'default',
     }
   },

5aba1541d7ca0709b90f5bae47f1e1cee6cbcda5
Surya Rajendhran <cielograndesurya@gmail.com>
2020-11-16 16:50:38 +0530

    Description

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,10 +10,10 @@ export default {
     title: 'Teacher Portal - Scholared',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { property: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
-        name: 'description',
+        property: 'description',
         content: 'The portal for teachers to access to Scholared tools',
       },
       {

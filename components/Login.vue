9ee0c40753b494e032fde41a10d4c42cda6bc7c8
Avinash MG <hatman290@gmail.com>
2020-11-17 11:35:18 +0530

    Login part1

---
+++ components/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="section">
+    <div class="columns">
+      <div class="column is-three-quarters"></div>
+      <div class="column is-one-quarter">
+          <div class="title is-4">Log In </div>
+        <div class="field">
+          <label for="username">E-mail</label>
+          <div class="control has-icons-left has-icons-right">
+            <input
+              class="input is-primary"
+              type="email"
+              placeholder="Your Username"
+              v-model="email"
+            />
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i
+            ></span>
+          </div>
+        </div>
+        <div class="field">
+          <label for="password">Password</label>
+          <div class="control has-icons-left has-icons-right">
+            <input
+              class="input is-primary"
+              type="text"
+              placeholder="Your Password"
+              v-model="password"
+            />
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i
+            ></span>
+          </div>
+        </div>
+        <div class="field">
+        <a href="#">Have trouble loggin in?</a>
+        </div>
+         <div class="field">
+            <button class="button is-primary is-fullwidth">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <login/>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

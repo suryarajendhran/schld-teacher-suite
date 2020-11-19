fc967b9f8e4a7365123983392f71d2b9253d0d43
Surya Rajendhran <cielograndesurya@gmail.com>
2020-11-19 13:42:37 +0530

    Tracking

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,10 +1,13 @@
 export default function ({ store, redirect, route }) {
+  console.log('Starting authentication')
   if (route.name != 'login' && !store.state.auth.authenticated) {
-    console.log(store.state.auth.authenticated)
+    // console.log(store.state.auth.authenticated)
+    console.log('Ending authentication')
     return redirect('/login')
   }
 
   if (route.name == 'login' && store.state.auth.authenticated) {
+    console.log('Ending authentication')
     return redirect('/')
   }
 }

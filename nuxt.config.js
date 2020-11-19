d4b8f1d983c3037a83d0f5f35a71c892a3e7723d
Surya Rajendhran <cielograndesurya@gmail.com>
2020-11-19 23:16:30 +0530

    Add fontawesome back

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,10 +29,10 @@ export default {
     ],
     script: [
       // { src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js' },
-      // {
-      //   src: 'https://kit.fontawesome.com/a135911755.js',
-      //   defer: true,
-      // },
+      {
+        src: 'https://kit.fontawesome.com/a135911755.js',
+        defer: true,
+      },
     ],
   },
 

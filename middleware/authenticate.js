c0ee5b8a0d64ae505391ba7ad1a7c7994cb9b745
Avinash MG <hatman290@gmail.com>
2020-12-03 13:39:50 +0530

    authenticate changed

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,7 +1,7 @@
 export default function ({ store, redirect, route }) {
   console.log('Starting authentication')
   console.log(store.state.auth.authenticated == false)
-  if (route.name != 'login' && !store.state.auth.authenticated) {
+  if (route.name != 'login' && route.name != 'register' && !store.state.auth.authenticated) {
     // console.log(store.state.auth.authenticated)
     console.log(
       `Ending authentication with token: ${store.state.auth.authenticated}`
